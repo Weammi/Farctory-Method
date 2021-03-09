5c4c2ae7b99fefe889314f727fcc2de485d6e41a
Weammi <75626639+Weammi@users.noreply.github.com>
2021-03-09 12:50:32 +0300

    Delete Tree.java

--- Tree.java
+++
@@ -1,9 +0,0 @@
-package Factory;
-
-public class Tree {
-    public static void main(String[] args) {
-        TangoFactory tangoFactory = new CreateFactory(1);
-        Tango tango = tangoFactory.createTango();
-        tango.showTango();
-    }
-}
